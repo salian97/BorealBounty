f95ab052015a2de01d06305bc9e9eafc16f2de4d
NattyEddy <99678920+NattyEddy@users.noreply.github.com>
2025-11-08 16:49:57 -0700

    Added Brainflow package and created a Brainflow Object with an empty script.

---
+++ Assets/Scripts/SimpleGetData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using brainflow;
+using brainflow.math;
+public class SimpleGetData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
