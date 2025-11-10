45d6c005d4484c3cbcdbb7619afcfe8448efa655
Zoe <zprefont@ualberta.ca>
2025-11-10 05:20:20 -0700

    Battle askes questions now!

--- Assets/Scripts/QuestionDictionary.cs
+++ Assets/Scripts/QuestionDictionary.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
 public class QuestionDictionary : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class QuestionDictionary : MonoBehaviour
     private Dictionary<string, List<QuestionData>> askedQuestionsDict;
     private List<QuestionData> questionsList;
     [SerializeField] private GameObject Admin;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +22,50 @@ public class QuestionDictionary : MonoBehaviour
         askedQuestionsDict = new Dictionary<string, List<QuestionData>>();
         questionsList = Admin.GetComponent<ReadCSV>().QuestionsList;
 
+        /*
         var groupedQuestions = questionsList.GroupBy(p => p.Difficulty);
+        foreach (var group in groupedQuestions)
+        {
+            Debug.Log("Meow");
+            questionDict.Add(group.Key, group.ToList());
+            Debug.Log(questionDict[group.Key]);
+        }
+        */
+
+        StartCoroutine(WaitForCSVData());
+    }
+
+    IEnumerator WaitForCSVData()
+    {
+        // Wait until questions are loaded
+        while (Admin.GetComponent<ReadCSV>().QuestionsList == null || Admin.GetComponent<ReadCSV>().QuestionsList.Count == 0)
+        {
+            Debug.Log("Waiting for CSV data to load...");
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        // Now the data is ready
+        questionsList = Admin.GetComponent<ReadCSV>().QuestionsList;
+        ProcessQuestions();
+    }
+
+    public void ProcessQuestions()
+    {
+        var groupedQuestions = questionsList.GroupBy(p => p.Difficulty);
+        
         foreach (var group in groupedQuestions)
         {
             questionDict.Add(group.Key, group.ToList());
         }
     }
+    
+    
 
     public QuestionData GetRandomQuestion(string diff)
     {
         if (questionDict.TryGetValue(diff, out var questions) && questions.Count > 0)
         {
-            int randomIndex = Random.Range(0, questions.Count);
+            int randomIndex = UnityEngine.Random.Range(0, questions.Count);
             Debug.Log($"Random question: {questions[randomIndex].Question} and difficulty {questions[randomIndex].Difficulty}");
 
             return questions[randomIndex];
