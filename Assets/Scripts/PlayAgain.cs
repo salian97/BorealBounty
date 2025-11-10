6445fe9cffeb7ae874c5c2035a5ba99a33bec45f
salian97 <salian@ualberta.ca>
2025-11-10 10:29:52 -0700

    Play Again Button

---
+++ Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour
+{
+    public void playAgain()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
