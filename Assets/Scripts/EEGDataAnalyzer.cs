859041da193c405d640a6e3aa1272f28a9964dfc
NattyEddy <99678920+NattyEddy@users.noreply.github.com>
2025-11-09 21:59:45 -0700

    Renamed to EEGDataAnalyzer, with a somewhat naive solution to our problem.

--- Assets/Scripts/EEGDataAnalyzer.cs
+++ Assets/Scripts/EEGDataAnalyzer.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 using brainflow;
 using brainflow.math;
 using System;
-public class SimpleGetData : MonoBehaviour
+using Unity.VisualScripting;
+using System.Runtime.Serialization;
+public class EEGDataAnalyzer : MonoBehaviour
 {
     private BoardShim board_shim = null;
     private int sampling_rate = 0;
     private int[] eeg_channels = null;
-    public string serial_port = "COMx"; // set the serial port of your NeuroPawn Knight Board here
+    private MLModel mindful_model = null;
 
-    private Tuple<double, double> alpha_band = new Tuple<double, double>(7.5, 12.5);
-    private Tuple<double, double> beta_band = new Tuple<double, double>(13.0, 30.0);
+    private double sumOfMindfulness = 0.0;
+    private double nMindfulness = 0.0;
+
+    [Inspectable]
+    public string serial_port = "COMx"; // set the serial port of your NeuroPawn Knight Board here
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +42,14 @@ public class SimpleGetData : MonoBehaviour
             sampling_rate = BoardShim.get_sampling_rate(board_id);
             eeg_channels = BoardShim.get_eeg_channels(board_id);
             Debug.Log("Brainflow streaming was started");
+
+            // Create ML model to predict Mindfulness 
+            BrainFlowModelParams mindfulness_params = new BrainFlowModelParams(
+                (int)BrainFlowMetrics.MINDFULNESS, (int)BrainFlowClassifiers.DEFAULT_CLASSIFIER);
+
+            mindful_model = new MLModel(mindfulness_params);
+            
+            mindful_model.prepare();
         }
         catch (BrainFlowError e)
         {
@@ -54,46 +67,14 @@ public class SimpleGetData : MonoBehaviour
         }
         int number_of_data_points = sampling_rate * 4;
         double[,] data = board_shim.get_current_board_data(number_of_data_points);
-        // Debug.Log("Num elements: " + data.GetLength(1));
-
+        
         // Get the average band powers
         Tuple<double[], double[]> avg_bands = DataFilter.get_avg_band_powers(data, eeg_channels, sampling_rate, true);
         double[] feature_vector = avg_bands.Item1;  // the actual average bands without std dev
 
-        // Create two ML models to predict Mindfulness and Restfulness 
-        BrainFlowModelParams mindfulness_params = new BrainFlowModelParams(
-            (int)BrainFlowMetrics.MINDFULNESS, (int)BrainFlowClassifiers.DEFAULT_CLASSIFIER);
-        BrainFlowModelParams restfulness_params = new BrainFlowModelParams(
-            (int)BrainFlowMetrics.RESTFULNESS, (int)BrainFlowClassifiers.DEFAULT_CLASSIFIER);
-
-        MLModel mindful_model = new MLModel(mindfulness_params);
-        MLModel restful_model = new MLModel(restfulness_params);
-
-        mindful_model.prepare();
-        restful_model.prepare();
-
-        // double mindfulness = mindful_model.predict(feature_vector)[0] * 100;
-        // double restfulness = restful_model.predict(feature_vector)[0] * 100;
-        
-        double[] m_arr = mindful_model.predict(feature_vector);
-        double[] r_arr = restful_model.predict(feature_vector);
-
-        int[] mi_arr = new int[m_arr.Length];
-        int[] ri_arr = new int[r_arr.Length];
-
-        for (int i = 0; i < feature_vector.Length; i++)
-        {
-            mi_arr[i] = (int)(m_arr[i] * 100);
-            ri_arr[i] = (int)(r_arr[i] * 100);
-        }
-
-        mindful_model.release();
-        restful_model.release();
-
-        // Debug.Log("Mindfulness: " + mindfulness + "\t Restfulness: " + restfulness);
-        Debug.Log("Mindfulness array: " + string.Join(", ", mi_arr));
-        Debug.Log("Restfulness array: " + string.Join(", ", ri_arr));
-
+        double mindfulness = mindful_model.predict(feature_vector)[0];
+        sumOfMindfulness += mindfulness;
+        ++nMindfulness;
     }
 
     // you need to call release_session and ensure that all resources correctly released
@@ -103,6 +84,8 @@ public class SimpleGetData : MonoBehaviour
         {
             try
             {
+                mindful_model.release();
+
                 board_shim.stop_stream();
                 board_shim.release_session();
             }
@@ -112,5 +95,13 @@ public class SimpleGetData : MonoBehaviour
             }
             Debug.Log("Brainflow streaming was stopped");
         }
+    }
+
+    // Get the average mindfulness score in percentage, measured from the time
+    // this script is first run.
+    public int GetMindfulnessScore()
+    {
+        if (nMindfulness == 0) { return 0; }
+        return (int) (sumOfMindfulness / nMindfulness * 100); // [%]
     }
 }
